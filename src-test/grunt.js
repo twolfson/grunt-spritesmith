f59be13332b289345265e563cbb54363798cdbf3
Todd Wolfson <todd@twolfson.com>
2012-08-18 21:35:10 -0700

    Added test files and making progress on the task

--- src-test/grunt.js
+++ src-test/grunt.js
@@ -1,10 +1,20 @@
 module.exports = function (grunt) {
+  grunt.file.mkdir('scratch');
+
   grunt.initConfig({
+    sprite: {
+      all: {
+        // source: 'test_sprites/*.{jpg,png}',
+        source: ['test_sprites/sprite1.png','test_sprites/sprite2.jpg','test_sprites/sprite3.png'],
+        destImg: 'scratch/sprite.png',
+        destCSS: 'scratch/sprite_positions.styl'
+      }
+    },
     test: {
       all: '*.test.js'
     }
   });
 
   grunt.loadTasks('../src');
-  grunt.registerTask('default', 'test');
+  grunt.registerTask('default', 'sprite test');
 };
--- src/grunt-spritesmith.js
+++ src/grunt-spritesmith.js
@@ -1,2 +1,37 @@
-var smith = require('spritesmith');
-console.log(smith);
+var spritesmith = require('spritesmith');
+
+module.exports = function (grunt) {
+  // Create a SpriteMaker function
+  function SpriteMaker() {
+    var data = this.data,
+        src = data.source,
+        img = data.destImg,
+        css = data.destCSS;
+
+    // Verify all properties are here
+    if (!src || !img || !css) {
+      return grunt.fail("grunt.sprite requires a source, destImg, and destCSS property");
+    }
+
+    // Load in all images from the src
+    var srcFiles = grunt.file.expand(src);
+
+    // Create an async callback
+    var cb = this.async();
+
+    // Run through spritesmith
+    spritesmith(srcFiles, function (err, result) {
+      // If an error occurred, callback with it
+      if (err) {
+        return cb(err);
+      }
+
+      // Otherwise, log the result
+      console.log(result);
+      cb(true);
+    });
+  }
+
+  // Export the SpriteMaker function
+  grunt.registerMultiTask('sprite', 'Spritesheet making utility', SpriteMaker);
+};
