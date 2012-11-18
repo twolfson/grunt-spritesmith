2dff37b93b39bb6cfa300eeddd18e986e7ac965f
Todd Wolfson <todd@twolfson.com>
2012-11-17 18:46:01 -0800

    Overrides tests are passing

--- src-test/grunt-spritesmith.test.js
+++ src-test/grunt-spritesmith.test.js
@@ -75,5 +75,43 @@ module.exports = {
 
     // Callback since we are done testing
     test.done();
+  },
+  'overrides': function (test) {
+    // Load in the images
+    // TODO: If this were BDD, we should be loading this into a canvas and doing a threshold comparison there (i.e. are the images 90% similar)
+    var expectedCanvasImage = fs.readFileSync(__dirname + '/expected_files/canvas.overrides.png', 'binary'),
+        // expectedGmImage = fs.readFileSync(__dirname + '/expected_files/gm.overrides.png', 'binary'),
+        expectedGmImage = 'Windows...',
+        actualImage = fs.readFileSync(__dirname + '/scratch/sprite.overrides.png', 'binary'),
+        matchesImage = expectedCanvasImage === actualImage || expectedGmImage === actualImage;
+
+    // Assert they are equal
+    test.ok(actualImage, 'Actual image does not match expected image');
+
+    // Load in the sprite positions
+    // TODO: If this were BDD, we would be asserting the same variables exist -- which means loading this into either Stylus or a meta-language
+    var expectedCoords = fs.readFileSync(__dirname + '/expected_files/sprite_positions.overrides.styl', 'utf8'),
+        actualCoords = fs.readFileSync(__dirname + '/scratch/sprite_positions.overrides.styl', 'utf8');
+
+    // Break up the expected coords into each line
+    var expectedLines = expectedCoords.split(/\n/g);
+
+    // Iterate over each line
+    expectedLines.forEach(function (line) {
+      // Trim the line
+      line = line.trim();
+
+      // If there is no line, skip it
+      if (!line) {
+        return;
+      }
+
+      // Assert that the line exists in actualCoords
+      var index = actualCoords.indexOf(line);
+      test.notEqual(index, -1, 'Line not found in actual coordinates: ' + line);
+    });
+
+    // Callback since we are done testing
+    test.done();
   }
 };
