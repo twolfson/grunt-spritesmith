dc01a309ac848aef993f45b6867da1aa777d1ade
Todd Wolfson <todd@twolfson.com>
2012-11-17 18:00:36 -0800

    Working on extension inference

--- src-test/grunt.js
+++ src-test/grunt.js
@@ -3,12 +3,27 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     sprite: {
-      all: {
+      'default': {
         // TODO: This order is forced due to png/jpg ordering. We should fix this.
         // src: 'test_sprites/*.{jpg,png}',
         src: ['test_sprites/sprite1.png','test_sprites/sprite2.jpg','test_sprites/sprite3.png'],
         destImg: 'scratch/sprite.png',
         destCSS: 'scratch/sprite_positions.styl'
+      },
+      'jpg,json': {
+        // TODO: This order is forced due to png/jpg ordering. We should fix this.
+        // src: 'test_sprites/*.{jpg,png}',
+        src: ['test_sprites/sprite1.png','test_sprites/sprite2.jpg','test_sprites/sprite3.png'],
+        destImg: 'scratch/sprite.jpg',
+        destCSS: 'scratch/sprite_positions.json'
+      },
+      'overrides': {
+        src: ['test_sprites/sprite1.png','test_sprites/sprite2.jpg','test_sprites/sprite3.png'],
+        destImg: 'scratch/sprite.overrides.png',
+        destCSS: 'scratch/sprite_positions.overrides.styl',
+        cssFormat: 'json',
+        imgFormat: 'jpg',
+        algorithm: 'alt-diagonal'
       }
     },
     test: {
--- src/grunt-spritesmith.js
+++ src/grunt-spritesmith.js
@@ -3,16 +3,25 @@ var spritesmith = require('spritesmith'),
     fs = require('fs'),
     path = require('path');
 
+function ext2format(formatObj) {
+  this.formatObj = formatObj;
+}
+ext2format.prototype = {
+  'get': function (filepath) {
+    return this
+  }
+};
+
 module.exports = function (grunt) {
   // Create a SpriteMaker function
   function SpriteMaker() {
     var data = this.data,
         src = data.src,
-        img = data.destImg,
-        css = data.destCSS;
+        destImg = data.destImg,
+        destCSS = data.destCSS;
 
     // Verify all properties are here
-    if (!src || !img || !css) {
+    if (!src || !destImg || !destCSS) {
       return grunt.fatal("grunt.sprite requires a source, destImg, and destCSS property");
     }
 
@@ -22,16 +31,41 @@ module.exports = function (grunt) {
     // Create an async callback
     var cb = this.async();
 
+    // Determine the format of the image
+    var imgFormat = data.imgFormat;
+
+    // If the format was not explicit
+    if (!imgFormat) {
+      // Grab the extension from the destImage
+      var cssExt = path.extname(destImg),
+          lowerCSSExt = cssExt.toLowerCase();
+
+      // Based on the lowerExt, define the imgFormat
+      switch (lowerCSSExt) {
+        case "styl":
+        case "stylus":
+          imgFormat = 'stylus';
+          break;
+
+        // By default, use json
+        case "js":
+        case "json":
+        default:
+          imgFormat =
+      }
+    }
+
     // Run through spritesmith
     // TODO: Specify algorithm specification
+    // TODO: Generate png or jpeg depending on extension
     spritesmith({'src': srcFiles}, function (err, result) {
       // If an error occurred, callback with it
       if (err) {
         return cb(err);
       }
 
-      // Otherwise, write out the result to img
-      fs.writeFileSync(img, result.image, 'binary');
+      // Otherwise, write out the result to destImg
+      fs.writeFileSync(destImg, result.image, 'binary');
 
       // Generate a listing of CSS variables
       var coordinates = result.coordinates,
@@ -62,7 +96,7 @@ module.exports = function (grunt) {
       var cssStr = json2css(cleanCoords, {'format': 'stylus'});
 
       // Write it out to the CSS file
-      fs.writeFileSync(css, cssStr, 'utf8');
+      fs.writeFileSync(destCSS, cssStr, 'utf8');
 
       // Callback
       cb(true);
