83a137998a8b986d38575c45d487af75b46173bc
Todd Wolfson <todd@twolfson.com>
2013-02-28 00:27:57 -0800

    Added tests for grunt@0.4. Everything is working sans grunt-retro \o//

---
+++ src-test/Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function (grunt) {
+  // Load in legacy config
+  require('./grunt')(grunt);
+
+  // Configure nodeunit as test
+  testConfig = grunt.config.get('test');
+  grunt.config.set('nodeunit', testConfig);
+
+  // Load in nodeunit
+  process.chdir('..');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  process.chdir(__dirname);
+
+  // Override default task
+  grunt.registerTask('default', ['sprite', 'nodeunit']);
+};
