a230cecb982b27f278855785d56caa04664677d9
Todd Wolfson <todd@twolfson.com>
2013-08-05 23:41:53 -0700

    Generating stylesheet and spritesheet

---
+++ docs/Gruntfile.js
@@ -0,0 +1,20 @@
+// Define our grunt file
+module.exports = function (grunt) {
+  // Configure the spritesheet
+  grunt.initConfig({
+    sprite: {
+      docs: {
+        src: ['fork.png', 'github.png', 'twitter.png'],
+        destImg: 'spritesheet.png',
+        destCSS: 'main.styl',
+        algorithm: 'binary-tree'
+      }
+    }
+  });
+
+  // Load in grunt-spritesmith
+  grunt.loadTasks('../tasks');
+
+  // Run the sprite task by default
+  grunt.registerTask('default', ['sprite']);
+};
