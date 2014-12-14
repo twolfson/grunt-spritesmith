e63b7880d6c3c58f1f4c5f251608280fc57f0582
Todd Wolfson <todd@twolfson.com>
2014-12-14 17:12:49 -0600

    Relocating test folder cleanup inside test

--- Gruntfile.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (grunt) {
-  // Configure our project
-  grunt.initConfig({
-    clean: ['src-test/scratch/']
-  });
-
-  // Load in our tasks
-  grunt.loadNpmTasks('grunt-contrib-clean');
-};
