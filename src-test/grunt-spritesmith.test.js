53c7307a1f22951762176f59df4f42e2a4f6a61c
Todd Wolfson <todd@twolfson.com>
2014-12-14 17:03:44 -0600

    Moved tests to BDD skeleton

--- src-test/grunt-spritesmith.test.js
+++ src-test/grunt-spritesmith.test.js
@@ -8,127 +8,144 @@ var actualDir = __dirname + '/scratch/';
 
 // Start our tests
 describe('grunt-spritesmith', function () {
-  it('default', function () {
-    // Load in the images
-    // TODO: If this were BDD, we should be loading this into a canvas and doing a threshold comparison there
-    //   (i.e. are the images 90% similar)
-    // var expectedCanvasImage = fs.readFileSync(expectedDir + 'canvas.png', 'binary');
-    // var expectedGmImage = fs.readFileSync(expectedDir + 'gm.png', 'binary');
-    var actualImage = fs.readFileSync(actualDir + 'sprite.png', 'binary');
-    // var matchesImage = expectedCanvasImage === actualImage || expectedGmImage === actualImage;
+  describe('converting a set of images', function () {
+    it('generates an image', function () {
+      // Load in the images
+      // TODO: If this were BDD, we should be loading this into a canvas and doing a threshold comparison there
+      //   (i.e. are the images 90% similar)
+      // var expectedCanvasImage = fs.readFileSync(expectedDir + 'canvas.png', 'binary');
+      // var expectedGmImage = fs.readFileSync(expectedDir + 'gm.png', 'binary');
+      var actualImage = fs.readFileSync(actualDir + 'sprite.png', 'binary');
+      // var matchesImage = expectedCanvasImage === actualImage || expectedGmImage === actualImage;
 
-    // Assert they are equal
-    // TODO: Perform more accurate assertion
-    assert(actualImage, 'Actual image does not match expected image');
+      // Assert they are equal
+      // TODO: Perform more accurate assertion
+      assert(actualImage, 'Actual image does not match expected image');
+    });
 
-    // Load in the sprite positions
-    // TODO: If this were BDD, we would be asserting the same variables exist -- which means loading this into either
-    //   Stylus or a meta-language
-    var expectedCoords = fs.readFileSync(expectedDir + 'sprite_positions.styl', 'utf8');
-    var actualCoords = fs.readFileSync(actualDir + 'sprite_positions.styl', 'utf8');
+    it('generates CSS variables', function () {
+      // Load in the sprite positions
+      // TODO: If this were BDD, we would be asserting the same variables exist -- which means loading this into either
+      //   Stylus or a meta-language
+      var expectedCoords = fs.readFileSync(expectedDir + 'sprite_positions.styl', 'utf8');
+      var actualCoords = fs.readFileSync(actualDir + 'sprite_positions.styl', 'utf8');
 
-    // Make sure the outputs match
-    assert.strictEqual(actualCoords, expectedCoords, 'Generated output doesn\'t match expected output.');
+      // Make sure the outputs match
+      assert.strictEqual(actualCoords, expectedCoords, 'Generated output doesn\'t match expected output.');
+    });
   });
 
-  it('jpg,json', function () {
-    // Load in the images
-    // TODO: If this were BDD, we should be loading this into a canvas and doing a threshold comparison there
-    //   (i.e. are the images 90% similar)
-    var actualImage = fs.readFileSync(actualDir + 'sprite.jpg', 'binary');
+  describe('generating a jpg and JSON', function () {
+    it('generates a jpg', function () {
+      // Load in the images
+      // TODO: If this were BDD, we should be loading this into a canvas and doing a threshold comparison there
+      //   (i.e. are the images 90% similar)
+      var actualImage = fs.readFileSync(actualDir + 'sprite.jpg', 'binary');
 
-    // Assert they are equal
-    // TODO: Perform more accurate assertion
-    assert(actualImage, 'Actual image does not match expected image');
+      // Assert they are equal
+      // TODO: Perform more accurate assertion
+      assert(actualImage, 'Actual image does not match expected image');
+    });
 
-    // Load in the sprite positions
-    // TODO: If this were BDD, we would be asserting the same variables exist -- which means loading this into either
-    //   Stylus or a meta-language
-    var expectedCoords = fs.readFileSync(expectedDir + 'sprite_positions.json', 'utf8');
-    var actualCoords = fs.readFileSync(actualDir + 'sprite_positions.json', 'utf8');
+    it('generates JSON', function () {
+      // Load in the sprite positions
+      // TODO: If this were BDD, we would be asserting the same variables exist -- which means loading this into either
+      //   Stylus or a meta-language
+      var expectedCoords = fs.readFileSync(expectedDir + 'sprite_positions.json', 'utf8');
+      var actualCoords = fs.readFileSync(actualDir + 'sprite_positions.json', 'utf8');
 
-    // Make sure the outputs match
-    assert.deepEqual(JSON.parse(actualCoords), JSON.parse(expectedCoords),
-      'Generated output doesn\'t match expected output.');
+      // Make sure the outputs match
+      assert.deepEqual(JSON.parse(actualCoords), JSON.parse(expectedCoords),
+        'Generated output doesn\'t match expected output.');
+    });
   });
 
-  it('overrides', function () {
-    // Load in the images
-    // TODO: If this were BDD, we should be loading this into a canvas and doing a threshold comparison there
-    //   (i.e. are the images 90% similar)
-    var actualImage = fs.readFileSync(actualDir + 'sprite.overrides.png', 'binary');
+  describe('running a task using overrides', function () {
+    it('generates an image', function () {
+      // Load in the images
+      // TODO: If this were BDD, we should be loading this into a canvas and doing a threshold comparison there
+      //   (i.e. are the images 90% similar)
+      var actualImage = fs.readFileSync(actualDir + 'sprite.overrides.png', 'binary');
 
-    // Assert they are equal
-    // TODO: Perform more accurate assertion
-    assert(actualImage, 'Actual image does not match expected image');
+      // Assert they are equal
+      // TODO: Perform more accurate assertion
+      assert(actualImage, 'Actual image does not match expected image');
+    });
 
-    // Load in the sprite positions
-    // TODO: If this were BDD, we would be asserting the same variables exist -- which means loading this into either
-    //   Stylus or a meta-language
-    var expectedCoords = fs.readFileSync(expectedDir + 'sprite_positions.overrides.styl', 'utf8');
-    var actualCoords = fs.readFileSync(actualDir + 'sprite_positions.overrides.styl', 'utf8');
+    it('generates coordinates', function () {
+      // Load in the sprite positions
+      // TODO: If this were BDD, we would be asserting the same variables exist -- which means loading this into either
+      //   Stylus or a meta-language
+      var expectedCoords = fs.readFileSync(expectedDir + 'sprite_positions.overrides.styl', 'utf8');
+      var actualCoords = fs.readFileSync(actualDir + 'sprite_positions.overrides.styl', 'utf8');
 
-    // Make sure the outputs match
-    assert.deepEqual(JSON.parse(actualCoords), JSON.parse(expectedCoords),
-      'Generated output doesn\'t match expected output.');
+      // Make sure the outputs match
+      assert.deepEqual(JSON.parse(actualCoords), JSON.parse(expectedCoords),
+        'Generated output doesn\'t match expected output.');
+    });
   });
 
-  it('nested', function () {
-    // Load in the coordinates and extract the path to the sprite file
-    var coords = fs.readFileSync(actualDir + '3/4/sprite_positions.styl', 'utf8');
+  describe('generating an image in a nested location', function () {
+    // TODO: Not entirely sure about the name of this test
+    it('resolves a collapsed path', function () {
+      // Load in the coordinates and extract the path to the sprite file
+      var coords = fs.readFileSync(actualDir + '3/4/sprite_positions.styl', 'utf8');
 
-    // Assert the path is the relative one we expect
-    assert.notEqual(coords.indexOf('../../nested/1/2/spritesheet.png'), -1);
+      // Assert the path is the relative one we expect
+      assert.notEqual(coords.indexOf('../../nested/1/2/spritesheet.png'), -1);
+    });
   });
+});
 
+// Edge cases
+describe('grunt-spritesmith', function () {
   // DEV: This is for testing an edge case -- don't let this strawman you in maintenance.
-  it('empty', function () {
-    // Setup
-    var img = fs.readFileSync(actualDir + 'empty/sprite.png', 'binary');
-    var coords = fs.readFileSync(actualDir + 'empty/sprite_positions.json', 'utf8');
+  describe('running a task with no images', function () {
+    it('generates an empty image', function () {
+      var img = fs.readFileSync(actualDir + 'empty/sprite.png', 'binary');
+      assert.strictEqual(img, '');
+    });
 
-    // Assert that an empty file and JSON blob are created
-    assert.strictEqual(img, '');
-    assert.strictEqual(coords, '{}');
+    it('generates no coordinates', function () {
+      var coords = fs.readFileSync(actualDir + 'empty/sprite_positions.json', 'utf8');
+      assert.strictEqual(coords, '{}');
+
+    });
   });
 
   // DEV: This is testing an edge case. CSS options are not critical for module functionality.
-  it('cssOpts', function () {
-    // Setup
-    var expectedCoords = fs.readFileSync(expectedDir + 'css_opts/sprite_positions.css', 'utf8');
-    var actualCoords = fs.readFileSync(actualDir + 'css_opts/sprite_positions.css', 'utf8');
-
-    // Make sure the outputs match
-    assert.strictEqual(actualCoords, expectedCoords, 'Generated output doesn\'t match expected output.');
+  describe('running a task with css options', function () {
+    it('uses the new selector', function () {
+      var expectedCoords = fs.readFileSync(expectedDir + 'css_opts/sprite_positions.css', 'utf8');
+      var actualCoords = fs.readFileSync(actualDir + 'css_opts/sprite_positions.css', 'utf8');
+      assert.strictEqual(actualCoords, expectedCoords, 'Generated output doesn\'t match expected output.');
+    });
   });
 
   // DEV: This is testing an edge case. A custom template is not critical for module functionality.
-  it('cssFunctionTemplate', function () {
-    // Setup
-    var expectedCoords = fs.readFileSync(expectedDir + 'sprite_positions_custom_function_template.styl', 'utf8');
-    var actualCoords = fs.readFileSync(actualDir + 'sprite_positions_custom_function_template.styl', 'utf8');
-
-    // Make sure the outputs match
-    assert.strictEqual(actualCoords, expectedCoords, 'Generated output doesn\'t match expected output.');
+  describe('running a task with a custom template function', function () {
+    it('uses the template', function () {
+      var expectedCoords = fs.readFileSync(expectedDir + 'sprite_positions_custom_function_template.styl', 'utf8');
+      var actualCoords = fs.readFileSync(actualDir + 'sprite_positions_custom_function_template.styl', 'utf8');
+      assert.strictEqual(actualCoords, expectedCoords, 'Generated output doesn\'t match expected output.');
+    });
   });
 
   // DEV: This is testing an edge case. A custom template is not critical for module functionality.
-  it('cssMustacheTemplate', function () {
-    // Setup
-    var expectedCoords = fs.readFileSync(expectedDir + 'sprite_positions_custom_mustache_template.styl', 'utf8');
-    var actualCoords = fs.readFileSync(actualDir + 'sprite_positions_custom_mustache_template.styl', 'utf8');
-
-    // Make sure the outputs match
-    assert.strictEqual(actualCoords, expectedCoords, 'Generated output doesn\'t match expected output.');
+  describe('running a task with a custom mustache template', function () {
+    it('uses the template', function () {
+      var expectedCoords = fs.readFileSync(expectedDir + 'sprite_positions_custom_mustache_template.styl', 'utf8');
+      var actualCoords = fs.readFileSync(actualDir + 'sprite_positions_custom_mustache_template.styl', 'utf8');
+      assert.strictEqual(actualCoords, expectedCoords, 'Generated output doesn\'t match expected output.');
+    });
   });
 
   // DEV: This is testing an edge case. A custom template is not critical for module functionality.
-  it('cssVarMap', function () {
-    // Setup
-    var expectedCoords = fs.readFileSync(expectedDir + 'css_var_map/sprite_positions.styl', 'utf8');
-    var actualCoords = fs.readFileSync(actualDir + 'css_var_map/sprite_positions.styl', 'utf8');
-
-    // Make sure the outputs match
-    assert.strictEqual(actualCoords, expectedCoords, 'Generated output doesn\'t match expected output.');
+  describe('running a task with custom CSS variable names', function () {
+    it('uses the new variable names', function () {
+      var expectedCoords = fs.readFileSync(expectedDir + 'css_var_map/sprite_positions.styl', 'utf8');
+      var actualCoords = fs.readFileSync(actualDir + 'css_var_map/sprite_positions.styl', 'utf8');
+      assert.strictEqual(actualCoords, expectedCoords, 'Generated output doesn\'t match expected output.');
+    });
   });
 });
