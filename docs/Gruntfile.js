c467e38435df5135e8934eb9669e6655d27d57a7
Todd Wolfson <todd@twolfson.com>
2014-12-14 19:11:07 -0600

    Cleaning up unused files

--- docs/Gruntfile.js
+++ docs/Gruntfile.js
@@ -8,42 +8,6 @@ module.exports = function (grunt) {
         destImg: 'spritesheet.png',
         destCSS: 'main.styl',
         algorithm: 'binary-tree'
-      },
-      css: {
-        src: ['fork.png', 'github.png', 'twitter.png'],
-        destImg: 'spritesheet.css.png',
-        destCSS: 'main.css'
-      },
-
-      'top-down': {
-        src: ['fork.png', 'github.png', 'twitter.png'],
-        destImg: 'top-down.png',
-        destCSS: 'top-down.css',
-        algorithm: 'top-down'
-      },
-      'left-right': {
-        src: ['fork.png', 'github.png', 'twitter.png'],
-        destImg: 'left-right.png',
-        destCSS: 'left-right.css',
-        algorithm: 'left-right'
-      },
-      diagonal: {
-        src: ['fork.png', 'github.png', 'twitter.png'],
-        destImg: 'diagonal.png',
-        destCSS: 'diagonal.css',
-        algorithm: 'diagonal'
-      },
-      'alt-diagonal': {
-        src: ['fork.png', 'github.png', 'twitter.png'],
-        destImg: 'alt-diagonal.png',
-        destCSS: 'alt-diagonal.css',
-        algorithm: 'alt-diagonal'
-      },
-      'binary-tree': {
-        src: ['fork.png', 'github.png', 'twitter.png'],
-        destImg: 'binary-tree.png',
-        destCSS: 'binary-tree.css',
-        algorithm: 'binary-tree'
       }
     }
   });
